e2b5b24b5d2a23b71ae57f9cc510463b976f5a08
MHD Ali <mdeeq.ali@gmail.com>
2017-02-17 12:38:18 +0000

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,15 @@ var redux = require('redux');
 
 console.log('Starting redux example.');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+}
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
   // state = state || {name: 'Anonymous'};
   switch (action.type) {
     case 'CHANGE_NAME':
@@ -10,6 +18,42 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
         ...state,
         name: action.name
       };
+      case 'ADD_HOBBY':
+        return {
+          ...state,
+          hobbies: [
+            ...state.hobbies,
+            {
+              id: nextHobbyId++,
+              hobby: action.hobby
+            }
+          ]
+        };
+
+        case 'REMOVE_HOBBY':
+          return {
+            ...state,
+            hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+          }
+
+        case 'ADD_MOVIE':
+          return {
+            ...state,
+            movies: [
+              ...state.movies,
+              {
+                id: nextMovieId++,
+                title: action.title,
+                genre: action.genre
+              }
+            ]
+          };
+
+          case 'REMOVE_MOVIE':
+            return {
+              ...state,
+              movies: state.movies.filter((movie) => movie.id !== action.id)
+            }
     default:
       return state;
   }
@@ -25,6 +69,8 @@ var unsubscribe = store.subscribe(()=> {
 
   console.log('Name is ', state.name);
   document.getElementById('app').innerHTML = state.name;
+
+  console.log('New state', store.getState());
 });
 // unsubscribe();
 
@@ -36,8 +82,39 @@ store.dispatch({
   name: 'Ali'
 });
 
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
 
 store.dispatch({
   type: "CHANGE_NAME",
   name: 'Amal'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Batman The Dark Night',
+  genre: 'Action Thriller'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Fast and Furious',
+  genre: 'Action Crime Thriller'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
 });
